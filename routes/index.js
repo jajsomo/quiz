0f7419e45b0f3e210b82c3235ed200575892d0de
jajsomo <jajsomo@gmail.com>
2015-08-02 20:19:43 +0200

    final del 7

--- routes/index.js
+++ routes/index.js
@@ -8,10 +8,14 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load);
 // registramos las rutas
 router.get('/quizes',		 				quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
+
+
 router.get('/author', function(req, res) {
   res.render('author');
 });
