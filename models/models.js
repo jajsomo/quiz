bfaeb68ae92f0823e743df8f6d6602baecb4e423
jajsomo <jajsomo@gmail.com>
2015-08-03 13:08:28 +0200

    Subida practica 8 6

--- models/models.js
+++ models/models.js
@@ -1,7 +1,7 @@
 var path = require('path');
 // Postgres DATABASE_URL = postgress://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
-var url =  DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var url =  process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
 var protocol = (url[1] || null);
 var dialect = (url[1] || null);
 var user = (url[2] || null);
@@ -10,7 +10,7 @@ var host = (url[4] || null);
 var port = (url[5] || null);
 var DB_name = (url[6] || null);
 var storage = process.env.DATABASE_STORAGE;
-console.log("bade de datos" + DB_name);
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
@@ -24,7 +24,8 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 	  omitNull: true		// solo Postgres
 	}
 );
-
+console.log("base de datos" + DATABASE_URL);
+console.log("base de datos" + process.env.DATABASE_URL);
 // Importar la definicion de la tabla Quiz en quiz.js
 //var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
