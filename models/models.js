bcfc65b89807b7135a2a0400e4691b854f41441d
jajsomo <jajsomo@gmail.com>
2015-08-03 13:14:25 +0200

    Subida practica 8 6

--- models/models.js
+++ models/models.js
@@ -25,6 +25,7 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 	  omitNull: true		// solo Postgres
 	}
 );
+console.log("base de datos" + sequelize);
 // Importar la definicion de la tabla Quiz en quiz.js
 //var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
