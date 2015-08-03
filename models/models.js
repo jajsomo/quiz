6a25ef9fc337cac675ec5d15f8957cb8f8896ed8
jajsomo <jajsomo@gmail.com>
2015-08-03 13:09:25 +0200

    Subida practica 8 6

--- models/models.js
+++ models/models.js
@@ -10,6 +10,8 @@ var host = (url[4] || null);
 var port = (url[5] || null);
 var DB_name = (url[6] || null);
 var storage = process.env.DATABASE_STORAGE;
+console.log("base de datos" + DATABASE_URL);
+console.log("base de datos" + process.env.DATABASE_URL);
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
@@ -24,8 +26,6 @@ var sequelize = new Sequelize(DB_name, user, pwd,
 	  omitNull: true		// solo Postgres
 	}
 );
-console.log("base de datos" + DATABASE_URL);
-console.log("base de datos" + process.env.DATABASE_URL);
 // Importar la definicion de la tabla Quiz en quiz.js
 //var quiz_path = path.join(__dirname, 'quiz');
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
