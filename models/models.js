06869314757bbadcbd43c0afd6695ced4f3a528a
jajsomo <jajsomo@gmail.com>
2015-08-03 11:32:58 +0200

    Subida practica 8 3

--- models/models.js
+++ models/models.js
@@ -1,17 +1,15 @@
 var path = require('path');
-// Postgres DATABASE_URL = postgres://lxguokvzdquapu:7WnMSPIKWpFblsvY04-AeKhQoa@ec2-54-83-10-210.compute-1.amazonaws.com:5432/d7jsvfapvbq09q
+// Postgres DATABASE_URL = postgress://user:passwd@host:port/database
 // SQLite DATABASE_URL = sqlite://:@:/
-//DATABASE_URL = "postgres://lxguokvzdquapu:7WnMSPIKWpFblsvY04-AeKhQoa@ec2-54-83-10-210.compute-1.amazonaws.com:5432/d7jsvfapvbq09q";
-//DATABASE_URL = "sqlite://:@:/";
-var url = DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name	 = (url[6]||null);
-var user	 = (url[2]||null);
-var pwd		 = (url[3]||null);
-var protocol = (url[1]||null);
-var dialect	 = (url[1]||null);
-var port	 = (url[5]||null);
-var host	 = (url[4]||null);
-var storage  = process.env.DATABASE_STORAGE;
+var url =  process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var host = (url[4] || null);
+var port = (url[5] || null);
+var DB_name = (url[6] || null);
+var storage = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
@@ -37,9 +35,9 @@ exports.Quiz = Quiz; // exportar definición de tabla Quiz
 sequelize.sync().then(function(){
 	// then(..) ejecuta el manejador una vez creada la table
 	Quiz.count().then(function(count){
-		if(count === 0){ // la tabla se inicializa solo si está vacia
-			Quiz.create({ pregunta : 'Capital de Italia', respuesta : 'Roma', tema:'humanidades'})
-			Quiz.create({ pregunta : 'Capital de Portugal', respuesta : 'Lisboa', tema:'humanidades'});//.then(function(){console.log('Bases de datos inicializada')});
+		if(count === 0){
+			Quiz.create({ pregunta : 'Capital de Italia', respuesta : 'Roma', tema: 'humanidades'});
+			Quiz.create({ pregunta : 'Capital de Portugal', respuesta : 'Lisboa', tema: 'humanidades'});
 		}
 	});
 });
