db053d152dc83178966f005c48bbe83d20944e5a
jajsomo <jajsomo@gmail.com>
2015-08-11 17:21:03 +0200

    Moderacion de comentarios

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload:quizId
+router.param('commentId', commentController.load); // autoload :commentId
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new); //formulario login
 router.post('/login', sessionController.create); // crear sesion
@@ -28,6 +29,8 @@ router.delete('/quizes/:quizId(\\d+)', 		sessionController.loginRequired, quizCo
 
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',	commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish', 
+											sessionController.loginRequired, commentController.publish);
 
 router.get('/author', 					quizController.author);
 
